4dfb2bf5ccbe4861e9858b133d70e355d1bccc80
Cyril Graze <cyrrill@users.noreply.github.com>
2021-10-31 18:38:28 -0600

    Install Express backend

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+dotenv.config()
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  const error = req.app.get('env') === 'development' ? err : {};
+  res.status(error.status || 500).send(error);
+});
+
+module.exports = app;
