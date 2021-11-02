4720f8fed9e1211db7e69e86f9191cbc68bba4cf
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-01 21:56:21 -0600

    Autopopulate restaurant dishes with plugin

--- backend/models/restaurant.js
+++ backend/models/restaurant.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const autopopulate = require('mongoose-autopopulate');
 const Schema = mongoose.Schema;
 
 const RestaurantSchema = new Schema({
     name: String,
     description: String,
     imageUrl: String,
-    dishes: [{type: Schema.Types.ObjectId, ref:'Dish'}],
+    dishes: [{ type: Schema.Types.ObjectId, ref:'Dish', autopopulate: true }],
 });
+
+RestaurantSchema.plugin(autopopulate);
 
 module.exports = mongoose.model('Restaurant', RestaurantSchema);
