6c8b973f0d42e6da1df58bc688b9d96ffaca676b
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-01 22:03:48 -0600

    Create cards array for home

---
+++ frontend/components/cards-array.js
@@ -0,0 +1,27 @@
+import { Card, Button, Row, Col } from 'react-bootstrap';
+
+export default function CardsArray(props) {
+
+  const cardsArray = props.data.restaurants.map(restaurant => (
+    <Col key={restaurant._id}>
+      <Card style={{height: "450px"}}>
+        <Card.Img variant="top" src={restaurant.imageUrl} style={{height: "170px"}} />
+        <Card.Body style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+          <div>
+            <Card.Title>{restaurant.name}</Card.Title>
+            <Card.Text>{restaurant.description}</Card.Text>
+          </div>
+          <div className="d-grid gap-2 b-0">
+            <Button size="lg" variant="dark" href={`/restaurants/${restaurant._id}`}>View</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </Col>
+  ))
+
+  return (
+    <Row xs={1} md={3}>
+     {cardsArray}
+    </Row>
+  )
+}
