8f2f975b1f7aa09ab382c25b7b2bc89988c39b74
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-01 22:01:26 -0600

    Add config

---
+++ frontend/config/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfb0Mmc1mHBJiI3KI40VBet8FfyQuA0qs",
+  authDomain: "full-stack-restaurant-app.firebaseapp.com",
+  projectId: "full-stack-restaurant-app",
+  storageBucket: "full-stack-restaurant-app.appspot.com",
+  messagingSenderId: "150574414743",
+  appId: "1:150574414743:web:960744440a9fa6118222d2"
+};
+
+export default firebaseConfig;
