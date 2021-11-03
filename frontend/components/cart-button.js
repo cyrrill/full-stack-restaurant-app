18a71948b1a278b6761287c8ff51ab603132e430
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-02 20:53:02 -0600

    Read cart data from store

--- frontend/components/cart-button.js
+++ frontend/components/cart-button.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import Image from 'next/image'
-import { OverlayTrigger, Popover, Button, Badge } from 'react-bootstrap'
+import { OverlayTrigger, Popover, Button, Badge, Table } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  addDish,
+  removeDish,
+  deleteDish,
+  emptyCart,
+  selectDishes,
+  selectTotal,
+  selectQuantity
+} from '../store/cart-slice'
 
 export default function CartButton() {
 
+  const quantity = useSelector(selectQuantity)
+  const dishesMap = useSelector(selectDishes)
 
+  const dishes = []
+  for (const [key, value] of Object.entries(dishesMap)) {
+    dishes.push({...value, _id: key})
+  }
+
+  const dishesArray = dishes.map(dish => (
+    <tr key={dish._id}>
+      <td><Image src={ dish.imageUrl } height="50px" width="50px" /></td>
+      <td>{ dish.name }</td>
+      <td>{ '$' + (dish.price / 100) }</td>
+      <td>{ dish.quantity }</td>
+      <td>{ '$' + (dish.quantity * dish.price / 100) }</td>
+    </tr>
+  ))
 
   return (
     <>
@@ -12,17 +38,21 @@ export default function CartButton() {
         trigger="click"
         placement="bottom"
         overlay={
-          <Popover id="popover-positioned-bottom">
-            {/* <Popover.Header as="h3">Shopping Cart</Popover.Header> */}
+          <Popover id="popover-positioned-bottom" style={{maxWidth: "450px"}}>
+            <Popover.Header>Shopping Cart</Popover.Header>
             <Popover.Body>
-              <strong>Holy guacamole!</strong> Check this info.
+              <Table style={{width: "350px"}}>
+                <tbody>
+                {dishesArray}
+                </tbody>
+              </Table>
             </Popover.Body>
           </Popover>
         }
       >
         <Button variant="dark" >
           <Image src="/images/cart.png" width="50px" height="50px" />
-          <Badge pill bg="danger">9</Badge>
+          <Badge pill bg="danger">{ quantity }</Badge>
         </Button>
       </OverlayTrigger>
     </>
