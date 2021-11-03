c590db74acd28be80414012a5b8d348bece6142a
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-03 17:14:07 -0600

    Add credit

--- frontend/store/storage.js
+++ frontend/store/storage.js
@@ -1,5 +1,6 @@
 import createWebStorage from "redux-persist/lib/storage/createWebStorage"
 
+// Credit to @rokinsky: https://github.com/vercel/next.js/discussions/15687#discussioncomment-45319
 const createNoopStorage = () => {
   return {
     getItem(_key) {
