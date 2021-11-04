d349842ae75c560f5c20c9b344aa0a97227086e2
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-03 23:00:14 -0600

    Implement Google signin

--- frontend/pages/auth/login.js
+++ frontend/pages/auth/login.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import router from 'next/router'
 import { Form, Button, Toast } from 'react-bootstrap';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 export default function Login() {
 
@@ -23,10 +23,57 @@ export default function Login() {
       .catch(() => setShowToast(true))
   }
 
+  function doGoogle() {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      router.push('/')
+    }).catch((error) => {
+      setShowToast(true)
+    });
+  }
+
+  const boxStyle = {
+    borderRadius: "0.5rem",
+    border: "1px solid #e0e0e0",
+    textAlign: "center",
+  }
+
+  const customBtnStyle =  {
+    display: "inline-block",
+    background: "white",
+    color: "#444",
+    width: "100%", //190px",
+    borderRadius: "5px",
+    border: "thin solid #888",
+    boxShadow: "1px 1px 1px grey",
+    whiteSpace: "nowrap",
+    marginBottom: "20px",
+    marginTop: "20px",
+    cursor: "pointer"
+  }
+
+  const spanIcon =  {
+    background: "url('/images/google.png') transparent 5px 50% no-repeat",
+    display: "inline-block",
+    verticalAlign: "middle",
+    width: "42px",
+    height: "42px",
+  }
+  const spanButtonText = {
+    display: "inline-block",
+    verticalAlign: "middle",
+    // paddingLeft: "42px",
+    paddingRight: "42px",
+    fontSize: "14px",
+    fontWeight: "bold",
+    fontFamily: "'Roboto', sans-serif",
+  }
+
   return (
     <main className="main">
-      <div className="col-md-7 col-lg-4 col-xl-4 mx-auto" style={{borderRadius: "0.5rem", border: "1px solid #e0e0e0"}}>
-        <Form className="py-5 px-5">
+      <div className="col-md-7 col-lg-4 col-xl-4 mx-auto" style={boxStyle}>
+        <Form className="pt-5 px-5">
           <div className="mb-3">
             <Form.Control type="email" id="email" placeholder="Email" required />
           </div>
@@ -37,6 +84,18 @@ export default function Login() {
             <Button variant="dark" onClick={doLogin} size="lg">Login</Button>
           </div>
         </Form>
+
+        <br/>
+        - or -
+
+        <div className="px-5">
+          <div id="customBtn" className="px-5" style={customBtnStyle} onClick={()=>doGoogle()}>
+            <span style={spanIcon}></span>
+            <span style={spanButtonText}>Google</span>
+          </div>
+        </div>
+        <br/>
+
       </div>
 
       <div className="toast-container position-absolute bottom-0 start-0 p-3" style={{zIndex: 11}}>
