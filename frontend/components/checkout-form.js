517a0c0cb37f680fcd33980377f64769d1750f47
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-04 12:58:17 -0600

    Add payment form validation

--- frontend/components/checkout-form.js
+++ frontend/components/checkout-form.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { useSelector } from 'react-redux'
 import { selectToken } from '../store/auth-slice'
-import { Spinner  } from "react-bootstrap";
+import { Spinner, Form  } from "react-bootstrap";
 import {
   PaymentElement,
   useStripe,
@@ -18,10 +18,35 @@ export default function CheckoutForm() {
   const stripe = useStripe();
   const elements = useElements();
 
-  const [message, setMessage] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   const token = useSelector(selectToken)
+
+  // https://stackoverflow.com/questions/285522/find-html-label-associated-with-a-given-input
+  const labels = document.getElementsByTagName('label');
+  for (var i = 0; i < labels.length; i++) {
+      if (labels[i].htmlFor != '') {
+          var elem = document.getElementById(labels[i].htmlFor);
+          if (elem) {
+              elem.label = labels[i];
+          }
+      }
+  }
+
+  const inputList = document.getElementsByTagName('input')
+  for (let item of inputList) {
+    item.addEventListener('change', event => {
+      if (event.target.value) {
+        event.target.classList.remove('input-invalid')
+        if (event.target.label) {
+          event.target.label.style.display = 'none'
+        }
+      }
+    })
+  }
+
+  let inputs, index;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,7 +55,19 @@ export default function CheckoutForm() {
       return;
     }
 
-    setIsLoading(true);
+    setIsLoading(true)
+    setMessage('')
+
+    inputs = document.getElementsByTagName('input');
+    for (index = 0; index < inputs.length; ++index) {
+        if (!inputs[index].value) {
+          inputs[index].classList.add('input-invalid')
+          if (inputs[index].label) {
+            inputs[index].label.style.display = 'block'
+          }
+        }
+    }
+    document.getElementById('payment-form').checkValidity()
 
     const { error } = await stripe.confirmPayment({
       elements,
@@ -49,7 +86,7 @@ export default function CheckoutForm() {
       },
     });
     if (error.type === "card_error" || error.type === "validation_error") {
-      setMessage(error.message);
+      //setMessage(error.message);
     } else {
       setMessage("An unexpected error occured.");
     }
@@ -75,13 +112,14 @@ export default function CheckoutForm() {
 
   return (
     <>
-    <form id="payment-form" onSubmit={handleSubmit}>
+    <form id="payment-form">
       <div className="group">
         <label>
           <span>Name</span>
         </label>
         <div>
           <input id="name" name="name" className="field" placeholder="Jane Doe" />
+          <label htmlFor="name" className="invalid-feedback" style={{display:"none"}}>Enter a name</label>
         </div>
       </div>
         <div className="group">
@@ -90,31 +128,35 @@ export default function CheckoutForm() {
         </label>
         <div>
           <input id="address-line1" name="address_line1" className="field" placeholder="77 Winchester Lane" />
+          <label htmlFor="address-line1" className="invalid-feedback" style={{display:"none"}}>Enter an address</label>
         </div>
         <label>
           <span>City</span>
         </label>
         <div>
           <input id="address-city" name="address_city" className="field" placeholder="Coachella" />
+          <label htmlFor="address-city" className="invalid-feedback" style={{display:"none"}}>Enter a city</label>
         </div>
         <label>
           <span>State</span>
         </label>
         <div>
           <input id="address-state" name="address_state" className="field" placeholder="CA" />
+          <label htmlFor="address-state" className="invalid-feedback" style={{display:"none"}}>Enter a state</label>
         </div>
         <label>
           <span>ZIP</span>
         </label>
         <div>
           <input id="address-zip" name="address_zip" className="field" placeholder="92236" />
+          <label htmlFor="address-zip" className="invalid-feedback" style={{display:"none"}}>Enter a zip code</label>
         </div>
       </div>
       <PaymentElement id="payment-element" />
       <br/>
       { isLoading
         ? <Spinner animation="border" variant="primary" />
-        : <button disabled={isLoading || !stripe || !elements} id="submit" style={styles}>
+        : <button disabled={isLoading || !stripe || !elements} id="submit" style={styles} onClick={(e) => handleSubmit(e)}>
             <span id="button-text">
               Pay now
             </span>
@@ -148,6 +190,28 @@ export default function CheckoutForm() {
       box-sizing: border-box;
       display: block;
       margin-bottom: 4px;
+    }
+    .input-invalid {
+      border-color: red;
+      box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(0, 0, 0, 0.02), 0 0 0 1px red;
+    }
+    .invalid-feedback {
+      box-sizing: border-box;
+      display: block;
+      height: 16.3636px;
+      margin-bottom: 4px;
+      margin-left: 0px;
+      margin-right:0px;
+      margin-top: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+      padding-right: 0px;
+      padding-top: 0px;
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+      font-size: 14.88px;
+      font-weight: 400!important;
+      line-height: 5px;
+      color: rgb(223, 27, 65);
     }
     `}</style>
   </>
