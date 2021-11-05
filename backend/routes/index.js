160fa5c957d8e62f5e049cb2e36f641cd7e204de
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-04 19:40:38 -0600

    Update API docs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,19 +1,8 @@
 const express = require('express');
-const firebaseAuth = require('../middleware/firebase-auth');
 const router = express.Router();
-const Dish  = require('../models/dish')
 
-/* GET index. */
 router.get('/', function(req, res, next) {
-  res.send({ title: 'Express' });
-});
-
-router.get('/secret', firebaseAuth, function(req, res, next) {
-  res.json({ title: req.user.email })
-});
-
-router.get('/dish', async function(req, res, next) {
-  res.json(await Dish.find({}));
+  res.send({ status: '200 OK' });
 });
 
 module.exports = router;
--- backend/routes/restaurants.js
+++ backend/routes/restaurants.js
@@ -4,12 +4,28 @@ const Dish  = require('../models/dish');
 const Restaurant = require('../models/restaurant')
 
 router.get('/', async function(req, res, next) {
+
+  // #swagger.tags = ['Restaurants']
+  // #swagger.description = 'Listing of all restaurants'
+
   res.json(await Restaurant.find());
 });
 
 router.get('/:restaurantId', async function(req, res, next) {
+
+  // #swagger.tags = ['Restaurants']
+  // #swagger.description = 'Get details for one restaurant by ID'
+  // #swagger.parameters['restaurantId'] = { description: 'Restaurant ID to lookup', type: 'string' }
+
   res.json(await Restaurant.findById(req.params.restaurantId));
 });
 
+router.get('/dish', async function(req, res, next) {
+
+  // #swagger.tags = ['Restaurants']
+  // #swagger.description = 'Listing of all dishes'
+
+  res.json(await Dish.find({}));
+});
 
 module.exports = router;
