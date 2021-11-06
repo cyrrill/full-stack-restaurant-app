2aad88afb3ea4fa6a0adb3610ea52936dd65d478
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-06 13:08:06 -0600

    Add firebase functions

---
+++ frontend/functions/index.js
@@ -0,0 +1,30 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+});
+
+
+/**
+ * Creates a document with ID -> uid in the `Users` collection.
+ *
+ * @param {Object} userRecord Contains the auth, uid and displayName info.
+ * @param {Object} context Details about the event.
+ */
+const createProfile = async(userRecord, context) => {
+
+  const { email, displayName, uid } = userRecord;
+
+  const res = await fetch(functions.config().backendUrl + '/users', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ email, displayName, uid }),
+  })
+  response.send(await res.json());
+};
+
+exports.authOnCreate = functions.auth.user().onCreate(createProfile);
