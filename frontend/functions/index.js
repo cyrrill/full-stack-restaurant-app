4e09e17550994fd0912e486b80ddd05187615bcb
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-07 02:54:42 -0600

    Update onAuth cloud function

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -4,28 +4,23 @@ import fetch from 'node-fetch';
 
 admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
-});
-
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//     functions.logger.info("Hello logs!", {structuredData: true});
+//     response.send("Hello from Firebase!");
+// });
 
 /**
  * Creates a document with ID -> uid in the `Users` collection.
  *
  * @param {Object} userRecord Contains the auth, uid and displayName info.
- * @param {Object} context Details about the event.
  */
-const createProfile = async(userRecord, context) => {
-
-  const { email, displayName, uid } = userRecord;
-
-  const res = await fetch(functions.config().backendUrl + '/users', {
+const createProfile = async userRecord => {
+  const { email, displayName, uid, photoURL } = userRecord;
+  const res = await fetch(functions.config().backend.url + '/users', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({ email, displayName, uid }),
+    body: JSON.stringify({ email, displayName, uid, photoURL }),
   })
-  response.send(await res.json());
 };
 
 export const authOnCreate = functions.auth.user().onCreate(createProfile);
