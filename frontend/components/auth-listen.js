3018b4063a775e7bc5b37c376711a933ba5cb505
Cyril Graze <cyrrill@users.noreply.github.com>
2021-11-06 18:34:00 -0600

    On load do firebase init on auth listen component

--- frontend/components/auth-listen.js
+++ frontend/components/auth-listen.js
@@ -1,16 +1,20 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { getAuth } from 'firebase/auth'
 import nookies from 'nookies'
 import { setToken } from '../store/auth-slice'
+import firebaseConfig from '../config/firebase';
 
 export default function AuthListen() {
 
   const dispatch = useDispatch()
 
-  useEffect(() => {
+  useEffect(async () => {
 
-    getAuth().onIdTokenChanged(nextOrObserver => {
+    const firebase = await import('firebase/app');
+    const auth = await import('firebase/auth');
+    const app = firebase.initializeApp(firebaseConfig)
+
+    auth.getAuth(app).onIdTokenChanged(nextOrObserver => {
       if (nextOrObserver) {
         const { accessToken, displayName, email } = nextOrObserver
         const tokenData = { accessToken, displayName, email }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
-import { initializeApp } from 'firebase/app'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from '../store/store'
-import firebaseConfig from '../config/firebase'
 import Layout from '../components/layout'
 import AuthListen from '../components/auth-listen'
-
-initializeApp(firebaseConfig)
 
 function MyApp({ Component, pageProps }) {
 
